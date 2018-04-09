01ba6e388db6fe89a994b6d90b0be8a3e871b65f
umang mittalgit config --global user.email mittal.umang@gmai.comgit config --global --listgit config --global user.name umang <mittal.umang@gmai.com>
2018-04-09 18:15:54 +0530

    priority arguments

--- concat.py
+++ concat.py
@@ -3,3 +3,10 @@ def concat_function(name,age):
 
 
 concat_function("umangdd",11)
+
+
+def priority_argument(name="umang",age="11"):
+    print("application is from ", name, "and the age is ", age)
+
+
+priority_argument("mittal")
