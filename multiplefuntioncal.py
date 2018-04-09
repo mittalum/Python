41602f98e94d5f8739a71b4df997d2cfee55778b
umang mittalgit config --global user.email mittal.umang@gmai.comgit config --global --listgit config --global user.name umang <mittal.umang@gmai.com>
2018-04-09 16:25:34 +0530

    hello.py updated

---
+++ multiplefuntioncal.py
@@ -0,0 +1,7 @@
+def my_funtion(str1,str2):
+    print(str1)
+    print(str2)
+
+
+my_funtion("first funtion call","first function second argument")
+my_funtion("function second call","second funtion second argument")
