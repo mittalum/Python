5954d0392f6841f1218a078f433aa5d6d856ddbf
umang mittalgit config --global user.email mittal.umang@gmai.comgit config --global --listgit config --global user.name umang <mittal.umang@gmai.com>
2018-04-09 14:59:48 +0530

    Initial commit

---
+++ funtions.py
@@ -0,0 +1,28 @@
+#funtions
+print("==============================")
+print("funtion call\n")
+print("==============================")
+print("hellow ")
+print ("hellow rold")
+def my_function():
+
+    print("this is my funtion")
+    print("a second string")
+
+my_function()
+
+# arguments in the function
+print("==============================")
+print("\narguments in the function\n")
+print("==============================")
+def my_funtion(str1,str2):
+    print(str1)
+    print(str2)
+
+
+my_funtion("umang","mittal")
+
+
+
+
+
