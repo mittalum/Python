9aeb1031023434b901a6595c157db0b280646da3
umang mittalgit config --global user.email mittal.umang@gmai.comgit config --global --listgit config --global user.name umang <mittal.umang@gmai.com>
2018-04-10 11:12:05 +0530

    if else elif statements

---
+++ ifelse.py
@@ -0,0 +1,11 @@
+
+check = " uttar pradesh"
+
+if check == "umang":
+    print("badiya")
+elif check == "mittal":
+    print("jhansi")
+elif check == " uttar pradesh":
+    print(" uttar pradesh")
+else:
+    print(" chal bhag galat hai yeh")
